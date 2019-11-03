d5d7bc0bc27d317e884bc13b3487ab125d17ead3
kymni <kymkimani@gmail.com>
2019-11-04 06:39:25 +1300

    add unit test

--- tests/Calendar.test.js
+++ tests/Calendar.test.js
@@ -6,3 +6,10 @@ it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Calendar />, div);
 });
+
+describe("functions", () => {
+  it("returns an array", () => {
+    const month = new Calendar();
+    expect(Array.isArray(month.arrayOfDays(2000))).toBe(true);
+  });
+});
